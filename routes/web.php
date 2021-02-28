40fcde7546dd5774740a7ef9276abf7bf385f37f
Admin <test@example,com>
2021-02-28 10:39:08 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\UsersController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/{name?}', function (Request $request) {
+    return $request->get('name');
+});
+
+// laravel 8
+Route::get('/users/index', [UsersController::class, 'index']);
+
+// before laravel 8
+Route::get('/users/index','UsersController@index');
